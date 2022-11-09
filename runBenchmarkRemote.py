84bc5cd2764336500c17af84250cfe11a8902ccb
gsoosk <farzadhabibii98@gmail.com>
2022-11-08 20:38:09 -0800

    added dummy run to cleanup after each experiment

--- runBenchmarkRemote.py
+++ runBenchmarkRemote.py
@@ -11,21 +11,21 @@ import paramiko
 
 servers = [
     {
-        "address":"129.114.109.1",
+        "address":"129.114.109.1", 
         "port": "8001",
-        "postgres": "5432",
+        "postgres": "9000",
         "partition": "1"
     },
     {
         "address":"129.114.109.112",
         "port": "8002",
-        "postgres": "5432",
+        "postgres": "9000",
         "partition": "2"
     },
     {
         "address":"129.114.108.105",
         "port": "8003",
-        "postgres": "5432",
+        "postgres": "9000",
         "partition": "3"
     }
 ]
@@ -36,21 +36,23 @@ client_address = "129.114.108.225"
 server_jar = "PaxosKV-server.jar"
 performance_jar = "PaxosKV-performance.jar"
 
-batch_siize_start = 4000
+batch_siize_start =300000
 batch_size_end = 600000
-batch_size_step = 4000
+batch_size_step = 10000 
 number_of_clients = 3
-benchmark_time = 20
+benchmark_time = 60
 result_folder = "./result"
-intervals = [300, 400, 500]
+intervals = [300]
+max_retry = 2
+
+dummy_interval = 300
+dummy_batch_size = 1000
 
 
 def runServers():
     s = []
     for server in servers:
         print(f'starting server {server["port"]}')
-        k = subprocess.Popen(["ssh", f'cc@{server["address"]}', 'pkill', 'java'])
-        k.wait()
         s.append(
             subprocess.Popen(["ssh", f'cc@{server["address"]}', "cd", "paxos", "&&" 
             "java", "-jar", server_jar, server["port"], server["postgres"], server["partition"], server["address"]])
@@ -65,46 +67,64 @@ def runBenchmark():
     if os.path.exists(result_folder):
         shutil.rmtree(result_folder)
     os.mkdir(result_folder)
+    print("preparing folders in remote...")
     subprocess.Popen(["ssh", f'cc@{client_address}', "sudo", "rm", "-rf", "~/paxos/result",]).wait()
     subprocess.Popen(["ssh", f'cc@{client_address}', "sudo", "mkdir", "~/paxos/result",]).wait()
 
     for interval in intervals:
+        print(f"interval is {interval}")
         for batch_size in range(batch_siize_start, batch_size_end, batch_size_step):
-            all_servers_process = runServers()
-            time.sleep(1) 
-
+            print(f"cleaning up previous run")
+            runClients(dummy_interval, dummy_batch_size, dummy=True)
+            time.sleep(10)
             print(f"starting {number_of_clients} benchmark clients for batch size {batch_size}, interval: {interval}")
-            clients = []
-            for i in range(number_of_clients):
-                client = subprocess.Popen(
+            runClients(interval, batch_size)
+    
+    subprocess.Popen(["scp", "-r", f'cc@{client_address}:~/paxos/result', "."]).wait
+
+def runClients(interval, batch_size, dummy=False):
+    clients = []
+    for i in range(number_of_clients):
+        result_file = f'~/paxos/result/result_partition{i + 1}.csv' if not dummy else f'/tmp/result_dummy{i + 1}.csv'
+        time = str(benchmark_time) if not dummy else str(10)
+        client = subprocess.Popen(
                     [
                         "ssh", f'cc@{client_address}', "cd", "paxos", "&&", "sudo",
                         "java", "-jar", performance_jar,
                         "--address", servers[i]['address'],
                         "--port", servers[i]['port'],
-                        "--benchmark-time", str(benchmark_time),
+                        "--benchmark-time", time,
                         "--throughput", "-1",
                         "--record-size", "255",
                         "--partition-id", str(i + 1),
                         "--batch-size", str(batch_size),
                         "--interval", str(interval),
                         "--timeout", str(700), 
-                        "--result-file", f'~/paxos/result/result_partition{i + 1}.csv'
+                        "--result-file", result_file,
+                        "--max-retry", str(max_retry) 
                     ]
                 )
-                clients.append(client)
-            for p in clients:
-                p.wait()
-            
-            for p in all_servers_process:
-                p.kill()
-            # time.sleep(3)
-    
-    subprocess.Popen(["scp", "-r", f'cc@{client_address}:~/paxos/result', "."]).wait
+        clients.append(client)
+    for p in clients:
+        p.wait()
 
 
-# all_servers_process = runServers()
-time.sleep(2)
+def killAllOpenProcesses():
+    print("killing opened processes")
+    subprocess.Popen(["ssh", f'cc@{client_address}', 'sudo', 'pkill', '--signal', 'SIGKILL',  'java']).wait()
+    for server in servers:
+        subprocess.Popen(["ssh", f'cc@{server["address"]}', 'sudo', 'pkill', '--signal', 'SIGKILL',  'java']).wait()
+
+def exitHandler(signum, frame):
+    killAllOpenProcesses()
+    exit(1)
+ 
+ 
+signal.signal(signal.SIGINT, exitHandler)
+killAllOpenProcesses()
+all_servers_process = runServers()
+time.sleep(5)
 runBenchmark()
+killAllOpenProcesses()
 # for p in all_servers_process:
 #     p.kill()
